e0a75eab75be4a2f568cb6e5d007b90f12703a6d
Andrea <andrearanica2004@gmail.com>
2026-07-11 15:30:50 +0200

    Added grid deformation

--- libs/world/world.cpp
+++ libs/world/world.cpp
@@ -10,10 +10,15 @@ void World::create_grid(GLuint* VAO) {
         for (int z = 0; z < width; z++) {
             int base = 6 * (x * width + z);
 
-            Vertex sw = Vertex(glm::vec3(x,  -1.0f,z));
-            Vertex se = Vertex(glm::vec3(x+1,-1.0f,z));
-            Vertex ne = Vertex(glm::vec3(x+1,-1.0f,z-1));
-            Vertex nw = Vertex(glm::vec3(  x,-1.0f, z-1));
+            Vertex sw = Vertex(glm::vec3(x,   0, z));
+            Vertex se = Vertex(glm::vec3(x+1, 0, z));
+            Vertex ne = Vertex(glm::vec3(x+1, 0, z-1));
+            Vertex nw = Vertex(glm::vec3(  x, 0, z-1));
+
+            sw.position.y = noise_generator.get_noise(sw.position.x / width, sw.position.z / height);
+            se.position.y = noise_generator.get_noise(se.position.x / width, se.position.z / height);
+            ne.position.y = noise_generator.get_noise(ne.position.x / width, ne.position.z / height);
+            nw.position.y = noise_generator.get_noise(nw.position.x / width, nw.position.z / height);
 
             vertices[base+0] = sw;
             vertices[base+1] = se;
@@ -25,7 +30,6 @@ void World::create_grid(GLuint* VAO) {
             float freq = 1;
             float amp = 1;
             float val = noise_generator.get_noise(sw.position.x / width, sw.position.z / height);
-            std::cout << "Noise: " << val << std::endl;
         }
     }
 
